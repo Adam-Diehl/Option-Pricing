c7f90da87825ab80ee1f0fe95ced148274a8bf9e
Adam-Diehl <adamtdiehl1996@gmail.com>
2017-06-08 00:08:41 -0400

    Add files via upload

---
+++ MCBlackScholes.c
@@ -0,0 +1,78 @@
+/*
+  Uses Monte Carlo simulation to price an option in a Black
+  Scholes world.
+
+  Author: Adam Diehl
+
+  Date: June 7, 2017
+*/
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include "MCBlackScholes.h"
+
+//Define parameters
+double S = 100;
+double K = 102;
+double r = 0.030485;
+double vol = 0.3392;
+double T = 0.5;
+double dt = 1/252;
+int N = 1000000;
+
+//Declare function
+double RNORM();
+
+int main() {
+
+//Seed RNG
+time_t t;
+srand((unsigned) time(&t));
+
+//Calculate constants
+double drift;
+  drift = (r-vol*vol*0.5)*T;
+double psp;
+  psp = vol*sqrt(T);
+double discount;
+  discount = exp(-r*T);
+double weight;
+  weight = 1/(N + 0.000);
+int progressIndicator;
+  progressIndicator = floor(0.1*N);
+
+//Calculate payoffs
+double CallPayoff[N];
+double PutPayoff[N];
+
+double CallPremium = 0;
+double PutPremium = 0;
+
+int i;
+for (i = 0; i < N; i = i+1) {
+  double TerminalPrice;
+    TerminalPrice = S*exp(drift + psp*RNORM());
+  CallPayoff[i] = (TerminalPrice - K);
+  PutPayoff[i] = (K - TerminalPrice);
+
+  if(CallPayoff[i] < 0){
+    CallPayoff[i] = 0;
+  }
+  if(PutPayoff[i] < 0){
+    PutPayoff[i] = 0;
+  }
+
+  CallPremium = CallPremium + CallPayoff[i];
+  PutPremium = PutPremium + PutPayoff[i];
+}
+
+CallPremium = CallPremium*discount*weight;
+PutPremium = PutPremium*discount*weight;
+
+printf("Call price = %f\n", CallPremium);
+printf("Put price = %f\n", PutPremium);
+
+return 0;
+}
---
+++ MCBlackScholes.h
@@ -0,0 +1,44 @@
+/*
+  Uses Monte Carlo simulation to price an option in a Black
+  Scholes world.
+
+  Header file contains the random normal generator
+
+  Author: Adam Diehl
+
+  Date: June 7, 2017
+*/
+
+//Mathematical constants
+#define Pi 3.14159265358979
+
+//NORMSINV constants
+#define  C1  (-7.784894002430293e-03)
+#define  C2  (-3.223964580411365e-01)
+#define  C3  (-2.400758277161838e+00)
+#define  C4  (-2.549732539343734e+00)
+#define  C5   4.374664141464968e+00
+#define  C6   2.938163982698783e+00
+#define  D1   7.784695709041462e-03
+#define  D2   3.224671290700398e-01
+#define  D3   2.445134137142996e+00
+#define  D4   3.754408661907416e+00
+
+//Define functions
+double RNORM();
+
+//Functions
+double RNORM() {
+  double p;
+  double e;
+  double u;
+  double q;
+  double x;
+
+  p = (((double)rand()+1)/((double)RAND_MAX+2));
+  q = sqrt(-2*log(p));
+  x = (((((C1*q+C2)*q+C3)*q+C4)*q+C5)*q+C6) / ((((D1*q+D2)*q+D3)*q+D4)*q+1);
+  e = 0.5 * erfc(-x/sqrt(2)) - p;
+  u = e * sqrt(2*Pi) * exp(0.5*x*x);
+  return (x - u/(1 + 0.5*x*u));
+}
